a443e8951f7d10b68383eda476fc6183bb6ba655
Jonathansvendsen <116103124+Jonathansvendsen@users.noreply.github.com>
2023-11-18 20:50:53 +0100

    Created mod.hjon

--- Script.js
+++
@@ -1,6 +0,0 @@
-Events.on(UnitDestroyEvent, event => {
-  // display toast on top of screen when the unit was a player
-  if(event.unit.isPlayer()){
-    Vars.ui.hudfrag.showToast("Pathetic.");
-  }
-})
